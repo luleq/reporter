963f4b972b4f81b8e75cd85719ba97f12b946579
anonymous <anonymous@example.com>
2018-12-11 16:04:27 +0100

    init

---
+++ report.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR
+
+ping github.com -c 1
+
+readonly RETURN_STATUS=$?
+
+touch "reports/$(date +%s)_${RETURN_STATUS}"
+
+git add .
+git -c user.name='anonymous' \
+	-c user.email='anonymous@example.com' \
+	commit -m "Report quick save $(date)"
+
+# git push
